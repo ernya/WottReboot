2117d068cbf05dc46b839be530c18c52ba600c58
Alexandre ERNY <alexandre.erny@epitech.eu>
2013-06-25 13:30:09 +0200

    Fixed portability problems (Specialized templates are a mess)

--- VertexBufferData.hpp
+++ VertexBufferData.hpp
@@ -34,6 +34,7 @@ protected:
 	VerticesType _type;
 	GLint	_components;
 public:
+
 	VertexBufferData(int components = 4, int size = 0) : _vertices(size)
 	{
 		throw std::runtime_error("Wrong template type for VertexBufferData");
@@ -43,7 +44,7 @@ public:
 	{
 		return _vertices[index];
 	}
-	
+
 	const T &operator[](int index) const
 	{
 		return _vertices.at(index);
@@ -69,3 +70,67 @@ public:
 		return _components;
 	}
 };
+/*template<>
+VertexBufferData<char>::VertexBufferData(int components, int size);
+
+template<>
+VertexBufferData<unsigned char>::VertexBufferData(int components, int size);
+
+template<>
+VertexBufferData<short>::VertexBufferData(int components, int size);
+
+template<>
+VertexBufferData<unsigned short>::VertexBufferData(int components, int size);
+
+template<>
+VertexBufferData<int>::VertexBufferData(int components, int size);
+
+template<>
+VertexBufferData<unsigned int>::VertexBufferData(int components, int size);
+
+template<>
+VertexBufferData<float>::VertexBufferData(int components, int size);
+
+template<>
+VertexBufferData<double>::VertexBufferData(int components, int size);*/
+
+template<>
+VertexBufferData<float>::VertexBufferData(int components, int size) : _vertices(size), _type(FLOAT), _components(components)
+{
+}
+
+template<>
+VertexBufferData<char>::VertexBufferData(int components, int size) : _vertices(size), _type(BYTE), _components(components)
+{
+}
+
+template<>
+VertexBufferData<unsigned char>::VertexBufferData(int components, int size) : _vertices(size), _type(UNSIGNED_BYTE), _components(components)
+{
+}
+
+template<>
+VertexBufferData<short>::VertexBufferData(int components, int size) : _vertices(size), _type(SHORT), _components(components)
+{
+}
+
+template<>
+VertexBufferData<unsigned short>::VertexBufferData(int components, int size) : _vertices(size), _type(UNSIGNED_SHORT), _components(components)
+{
+}
+
+
+template<>
+VertexBufferData<int>::VertexBufferData(int components, int size) : _vertices(size), _type(INT), _components(components)
+{
+}
+
+template<>
+VertexBufferData<unsigned int>::VertexBufferData(int components, int size) : _vertices(size), _type(UNSIGNED_INT), _components(components)
+{
+}
+
+template<>
+VertexBufferData<double>::VertexBufferData(int components, int size) : _vertices(size), _type(DOUBLE), _components(components)
+{
+}
