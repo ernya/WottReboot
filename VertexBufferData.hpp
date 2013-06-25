2d031b533ece295f4f44d5a2e16578bb5fa7f472
Alexandre ERNY <alexandre.erny@epitech.eu>
2013-06-25 13:46:51 +0200

    Mystic bug-fix to allow g++ to compile o_O

--- VertexBufferData.hpp
+++ VertexBufferData.hpp
@@ -70,67 +70,46 @@ public:
 		return _components;
 	}
 };
-/*template<>
-VertexBufferData<char>::VertexBufferData(int components, int size);
+
+
 
 template<>
-VertexBufferData<unsigned char>::VertexBufferData(int components, int size);
-
-template<>
-VertexBufferData<short>::VertexBufferData(int components, int size);
-
-template<>
-VertexBufferData<unsigned short>::VertexBufferData(int components, int size);
-
-template<>
-VertexBufferData<int>::VertexBufferData(int components, int size);
-
-template<>
-VertexBufferData<unsigned int>::VertexBufferData(int components, int size);
-
-template<>
-VertexBufferData<float>::VertexBufferData(int components, int size);
-
-template<>
-VertexBufferData<double>::VertexBufferData(int components, int size);*/
-
-template<>
-VertexBufferData<float>::VertexBufferData(int components, int size) : _vertices(size), _type(FLOAT), _components(components)
+inline VertexBufferData<float>::VertexBufferData(int components, int size) : _vertices(size), _type(FLOAT), _components(components)
 {
 }
 
 template<>
-VertexBufferData<char>::VertexBufferData(int components, int size) : _vertices(size), _type(BYTE), _components(components)
+inline VertexBufferData<char>::VertexBufferData(int components, int size) : _vertices(size), _type(BYTE), _components(components)
 {
 }
 
 template<>
-VertexBufferData<unsigned char>::VertexBufferData(int components, int size) : _vertices(size), _type(UNSIGNED_BYTE), _components(components)
+inline VertexBufferData<unsigned char>::VertexBufferData(int components, int size) : _vertices(size), _type(UNSIGNED_BYTE), _components(components)
 {
 }
 
 template<>
-VertexBufferData<short>::VertexBufferData(int components, int size) : _vertices(size), _type(SHORT), _components(components)
+inline VertexBufferData<short>::VertexBufferData(int components, int size) : _vertices(size), _type(SHORT), _components(components)
 {
 }
 
 template<>
-VertexBufferData<unsigned short>::VertexBufferData(int components, int size) : _vertices(size), _type(UNSIGNED_SHORT), _components(components)
+inline VertexBufferData<unsigned short>::VertexBufferData(int components, int size) : _vertices(size), _type(UNSIGNED_SHORT), _components(components)
 {
 }
 
 
 template<>
-VertexBufferData<int>::VertexBufferData(int components, int size) : _vertices(size), _type(INT), _components(components)
+inline VertexBufferData<int>::VertexBufferData(int components, int size) : _vertices(size), _type(INT), _components(components)
 {
 }
 
 template<>
-VertexBufferData<unsigned int>::VertexBufferData(int components, int size) : _vertices(size), _type(UNSIGNED_INT), _components(components)
+inline VertexBufferData<unsigned int>::VertexBufferData(int components, int size) : _vertices(size), _type(UNSIGNED_INT), _components(components)
 {
 }
 
 template<>
-VertexBufferData<double>::VertexBufferData(int components, int size) : _vertices(size), _type(DOUBLE), _components(components)
+inline VertexBufferData<double>::VertexBufferData(int components, int size) : _vertices(size), _type(DOUBLE), _components(components)
 {
 }
